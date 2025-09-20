c81e305023ddad782b320ce4e40c92e976c3aa87
bharath <bharathac7@gmail.com>
2025-09-20 21:08:37 +0530

    new

--- public/js/mainGame/Mario.js
+++ public/js/mainGame/Mario.js
@@ -1,11 +1,6 @@
 function Mario() {
   var gameUI = GameUI.getInstance();
 
-  // --- Tunables ---
-  // Make this more negative for a higher jump.
-  this.JUMP_IMPULSE = -38;   // was typically around -12 to -14
-  this.AIR_JUMP_LOCK = true; // keep single-jump behavior
-
   this.type = 'small';
   this.x;
   this.y;
@@ -22,7 +17,6 @@ function Mario() {
   this.frame = 0;
 
   var that = this;
-  var marioSprite;
 
   this.init = function() {
     that.x = 10;
@@ -32,37 +26,9 @@ function Mario() {
     marioSprite.src = 'images/mario-sprites.png';
   };
 
-  // Call this from your input handler instead of setting velY directly
-  this.jump = function() {
-    if (that.AIR_JUMP_LOCK) {
-      if (!that.jumping && that.grounded) {
-        that.velY = that.JUMP_IMPULSE;
-        that.jumping = true;
-        that.grounded = false;
-      }
-    } else {
-      // (optional) allow mid-air jump boosts
-      that.velY = that.JUMP_IMPULSE;
-      that.jumping = true;
-      that.grounded = false;
-    }
-  };
-
-  // Optional: quick way to scale jump height at runtime (e.g., powerups)
-  this.setJumpHeight = function(multiplier) {
-    // multiplier > 1 makes it higher; e.g., 1.2 = +20% jump height
-    that.JUMP_IMPULSE = -Math.abs(that.JUMP_IMPULSE) * multiplier;
-  };
-
   this.draw = function() {
     that.sX = that.width * that.frame;
-    gameUI.draw(
-      marioSprite,
-      that.sX, that.sY,
-      that.width, that.height,
-      that.x, that.y,
-      that.width, that.height
-    );
+    gameUI.draw(marioSprite, that.sX, that.sY, that.width, that.height, that.x, that.y, that.width, that.height);
   };
 
   this.checkMarioType = function() {
@@ -98,4 +64,5 @@ function Mario() {
     that.y = canv.height - 40;
     that.frame = 0;
   };
+  
 }
